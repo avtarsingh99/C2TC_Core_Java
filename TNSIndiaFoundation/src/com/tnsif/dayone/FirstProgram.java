0cab30d1c02cab90734779ebd26c687e73b0d42e
Avtar <avtarsingh160701@gmail.com>
2024-02-29 15:42:45 +0530

    First commit

---
+++ TNSIndiaFoundation/src/com/tnsif/dayone/FirstProgram.java
@@ -0,0 +1,19 @@
+package com.tnsif.dayone;
+
+import java.util.Scanner;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello again !");
+		Scanner sc = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter 2 Numbers:-");
+		a= sc.nextInt();
+		b= sc.nextInt();
+		c= a+b;
+		System.out.println("Sum of "+a+" and "+b+" is :- "+c);
+
+	}
+
+}
